ce7219a63b126eba9ec73087df94a9edf6cb95d8
nandaanumolu <nandakishore087@gmail.com>
2021-01-24 23:26:46 +0530

    asking for search bar

--- applyuni/Portal/views/stdportal.py
+++ applyuni/Portal/views/stdportal.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import render,redirect,HttpResponseRedirect
 from django.http import HttpResponse
-
+from Portal.models.universityinfo import University
 
 def stdhome(request):
-    return render(request,'student_portal/studenthome.html')
+    value={'Name':request.session['Firstname']}
+    data={'value':value}
+    return render(request,'student_portal/studenthome.html',data)
 def stdnav(request):
     return render(request,'student_portal/studentnav.html')
 def stdsaved(request):
     return render(request,'student_portal/studentsaved.html')
 def stdunivlist(request):
-    return render(request,'student_portal/university_cards.html')
+    total_univs=University.objects.all()
+    data={}
+    data['total_univs']=total_univs
+    return render(request,'student_portal/university_cards.html',data)
